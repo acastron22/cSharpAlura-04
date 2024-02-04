0b7ec0b03e3ec88c5646e1d589964b26f31cd0df
Ariel <arielcastron22@hotmail.com>
2024-02-04 19:46:45 -0300

    first commit

---
+++ ScreenSound-04/Program.cs
@@ -0,0 +1,6 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    string resposta = await client.
+        GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+}
