2f435c37975bae47e91426cde48b65a87cbac27f
Ariel <arielcastron22@hotmail.com>
2024-02-04 20:53:44 -0300

    deserialização dos dados

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,10 +1,16 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using System.Text.Json;
+using AluraCSharp4.Modelos;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.
             GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        
+
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
     }
     catch (Exception e)
     {
