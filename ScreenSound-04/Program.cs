c0832d6f7e46477155620d983e07004c91f2d900
Ariel <arielcastron22@hotmail.com>
2024-02-04 20:03:36 -0300

    desafio 1-1

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,9 +1,14 @@
-﻿using (HttpClient client = new HttpClient())
+﻿
+/*
+ *      Escrever um programa que faça uam requisição para a API de games CheapShark e mostre na tela a lista de
+ *      promoções cadastrada na ferramenta (Você pode utilizar o endpoint: https://www.cheapshark.com/api/1.0/deals).
+ */
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.
-            GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+            GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
         Console.WriteLine(resposta);
     }
     catch (Exception e)
