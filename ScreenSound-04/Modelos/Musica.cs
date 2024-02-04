c09bcea2ebb0f07f7692dd6377919e25870b145f
Ariel <arielcastron22@hotmail.com>
2024-02-04 20:44:03 -0300

    criação da classe das músicas

---
+++ ScreenSound-04/Modelos/Musica.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AluraCSharp4.Modelos;
+
+public class Musica
+{
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    
+    
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    
+    
+    [JsonPropertyName("duration_ms")]
+    public int? Duracao { get; set; }
+    
+    
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Nome da música: {Nome}");
+        Console.WriteLine($"Duracao: {Duracao / 1000} segundos");
+        Console.WriteLine($"Genero: {Genero}");
+    }
+    
+}
